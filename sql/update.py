26586456cbcc700b39541dafbaef97ebc61b66e4
yaojinsong <924602776@qq.com>
2024-06-02 22:46:20 +0800

    'refactor:update方法优化'

--- sql/update.py
+++ sql/update.py
@@ -53,42 +53,12 @@ def updateDay(id):
 
 # 更新元宝数
 def updateYuanBaoSum(id, yuanbaoSum):
-    db = pymysql.connect(host='localhost', user='root', password='123456', database='sgs', charset='utf8')
-    # 使用 cursor() 方法创建一个游标对象 cursor
-    cursor = db.cursor()
-    try:
-        # sql = "update usertest set yuanbaoSum = " + str(yuanbaoSum) + " where id = '" + id + "'";
-        sql = "update usertest set yuanbaoSum = '" + yuanbaoSum + "'" + " where id = '" + id + "'";
-        print(sql)
-        # 运行sql语句
-        cursor.execute(sql)
-        # 修改
-        db.commit()
-        # 关闭游标
-        cursor.close()
-        # 关闭连接
-        db.close()
-        print("victory!")
-    except:
-        print("false")
+    sql = f"update usertest set yuanbaoSum = '{yuanbaoSum}' " \
+          f"where id = '{id}'".format(yuanbaoSum, id);
+    excuteUpdateSql(sql)
 
 
 # 更新天数
 def updateStatus(id):
-    db = pymysql.connect(host='localhost', user='root', password='123456', database='sgs', charset='utf8')
-    # 使用 cursor() 方法创建一个游标对象 cursor
-    cursor = db.cursor()
-    try:
-        sql = "update usertest set `status` = 2,`check` = `check` + 1 where id = '" + id + "'";
-        print(sql)
-        # 运行sql语句
-        cursor.execute(sql)
-        # 修改
-        db.commit()
-        # 关闭游标
-        cursor.close()
-        # 关闭连接
-        db.close()
-        print("victory!")
-    except:
-        print("false")
+    sql = f"update usertest set `status` = 2,`check` = `check` + 1 where id = '{id}'".format(id)
+    excuteUpdateSql(sql)
